ed609804d1d7058462bf38b3b2bcf102b772f22d
Emmanuel Olive <olive.emmanuel@gmail.com>
2016-06-01 10:09:01 +0200

    Add twig management.

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -5,6 +5,7 @@ namespace Moulino\Framework\Controller;
 use Moulino\Framework\Service\Container;
 use Moulino\Framework\View\EngineInterface;
 use Moulino\Framework\Http\Response;
+use Moulino\Framework\Core\Exception\TemplateNotFoundException;
 
 use Moulino\Framework\Validation\Constraint\ConstraintViolationListInterface;
 	
@@ -28,9 +29,18 @@ abstract class AbstractController implements ControllerInterface
 
 	public function renderView($view, $vars = array()) {
 		$folder = ucfirst(strstr($view, ':', true));
-		$file = substr(strstr($view, ':'), 1).'.php';
+		$file = substr(strstr($view, ':'), 1);
 
 		$filepath = VIEW.DS.$folder.DS.$file;
+
+		// add the extension
+		if(file_exists($filepath.'.html')) {
+			$filepath .= '.html';
+		} else if(file_exists($filepath.'.php')) {
+			$filepath .= '.php';
+		} else {
+			throw new TemplateNotFoundException("No file was found with extension 'html' or 'php'.");
+		}
 		
 		return $this->view->render($filepath, $vars);
 	}
--- src/View/Engine.php
+++ src/View/Engine.php
@@ -10,6 +10,7 @@ class Engine implements EngineInterface
 {
 	private $authenticator;
 	private $mode;
+	private $twig; // can be used if the twig module has been installed
 
 	const NOT_FOUND_MESSAGE = "The template file %s was not found.";
 
@@ -17,16 +18,24 @@ class Engine implements EngineInterface
 
 		$this->authenticator = $authenticator;
 		$this->mode = $mode;
+
+		$this->twig = (file_exists(VENDOR.DS."twig/twig")) ? true : false;
 	}
 
 	public function render($filepath, $vars = array()) {
+		$vars = array_merge($vars, [$this->mode], $this->authenticator->getAuthInfo());
+
+		if(true === $this->twig) {
+			$loader = new \Twig_Loader_Filesystem(dirname($filepath));
+			$twig = new \Twig_Environment($loader, ['cache' => CACHE.DS.'twig']);
+			return $twig->render(basename($filepath), $vars);
+		}
+
 		if(!file_exists($filepath)) {
 			$message = sprintf(self::NOT_FOUND_MESSAGE, $filepath);
 			throw new TemplateNotFoundException($message);
 		}
 
-		$mode = $this->mode;
-		extract($this->authenticator->getAuthInfo());
 		extract($vars);
 
 		ob_start();
